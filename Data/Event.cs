1c8c72f81c6e55d53eb3723909b36f2fa83b722c
Jeremy Kinyua <jkinyua-ops@outlook.com>
2025-05-15 11:33:44 +0300

    update file

--- Data/Event.cs
+++ Data/Event.cs
@@ -6,9 +6,8 @@ namespace TicketingApp.Data
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
         public DateTime Date { get; set; }
         public decimal Price { get; set; }
-        public int AvailableTickets { get; set; }
+        // Add any other properties you need for your Event
     }
 }
